c993b059361f9274dc99c9a7acc6b6ab6e4501ee
Idan_A <88105799+Idan-Ay@users.noreply.github.com>
2025-07-31 12:02:40 +0000

    Flakes dir fix Root boot specified

--- system.nix
+++ system.nix
@@ -52,4 +52,14 @@
   };
 
   security.sudo.enable = true;
+
+  boot.initrd.supportedFilesystems = [ "ext4" ];
+  fileSystems."/" = {
+    device = "/dev/nvme0n1p5";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/nvme0n1p1";
+    fsType = "vfat";
+  };
 }
