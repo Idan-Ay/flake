003e23a2c496625239ae2765fd42ac4472e6431e
Idan_A <88105799+Idan-Ay@users.noreply.github.com>
2025-08-06 11:14:52 +0000

    Removed caelestia shell

--- flake.nix
+++ flake.nix
@@ -5,28 +5,18 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager";
-
-    caelestia-shell.url = "path:./caelestia-shell";
-    caelestia-shell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, home-manager, caelestia-shell, ... }: {
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }: {
 
     nixosConfigurations.idan-pc-l = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./system.nix
         home-manager.nixosModules.home-manager
-        caelestia-shell.nixosModules.default
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = false;
-
-          services.caelestia-shell.enable = true;
-          services.caelestia-shell.config = {
-            bar.workspaces.shown = 4;
-            dashboard.weatherLocation = "50.9289883,6.3601925";
-          };
         }
       ];
     };
