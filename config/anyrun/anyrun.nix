0549b1c276b05e8248c7cac82019187a6d585e78
Idan-Ay <idan.ayache@gmail.com>
2025-08-16 18:38:17 +0200

    Maybe Fixed anyrun

--- config/anyrun/anyrun.nix
+++ config/anyrun/anyrun.nix
@@ -1,27 +1,29 @@
-{
+let
+  anyrunFlake = builtins.getFlake "github:anyrun-org/anyrun";
+in {
   programs.anyrun = {
-    enable = true;
-    config = {
-      x = { fraction = 0.5; };
-      y = { fraction = 0.3; };
-      width = { fraction = 0.3; };
-      hideIcons = false;
-      ignoreExclusiveZones = false;
-      layer = "overlay";
-      hidePluginInfo = false;
-      closeOnClick = false;
-      showResultsImmediately = false;
-      maxEntries = null;
+      enable = true;
+      config = {
+        x = { fraction = 0.5; };
+        y = { fraction = 0.3; };
+        width = { fraction = 0.3; };
+        hideIcons = false;
+        ignoreExclusiveZones = false;
+        layer = "overlay";
+        hidePluginInfo = false;
+        closeOnClick = false;
+        showResultsImmediately = false;
+        maxEntries = null;
+      };
+
+      plugins = [
+        anyrunFlake.packages.${pkgs.system}.applications
+        anyrunFlake.packages.${pkgs.system}.websearch
+        anyrunFlake.packages.${pkgs.system}.dictionary
+        anyrunFlake.packages.${pkgs.system}.files
+        anyrunFlake.packages.${pkgs.system}.translate
+      ];
+
+      extraCss = builtins.readFile ./style.css;
     };
-
-    plugins = [
-      anyrunFlake.packages.${pkgs.system}.applications
-      anyrunFlake.packages.${pkgs.system}.websearch
-      anyrunFlake.packages.${pkgs.system}.dictionary
-      anyrunFlake.packages.${pkgs.system}.files
-      anyrunFlake.packages.${pkgs.system}.translate
-    ]
-
-    extraCss = builtins.readFile ./style.css;
-  };
 }
