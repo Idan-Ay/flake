22c014037786aefa1c321f1d815d7bd8aa67cc73
Idan-Ay <idan.ayache@gmail.com>
2025-08-20 16:06:57 +0200

    Added back nvidia drivers

--- packages/drivers.nix
+++ packages/drivers.nix
@@ -3,22 +3,22 @@
 {
   services.xserver.wacom.enable = true;
 
-  # # Use the official proprietary NVIDIA driver
-  # hardware.nvidia = {
-  #   modesetting.enable = true;
-  #   powerManagement.enable = true;
-  #   open = false;  # use proprietary driver
-  #   nvidiaSettings = true;
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
+  # Use the official proprietary NVIDIA driver
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    open = false;  # use proprietary driver
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
-  # services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
-  # # Recommended for Wayland (Hyprland, Sway, etc.)
-  # environment.variables = {
-  #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  #   WLR_NO_HARDWARE_CURSORS = "1";  # workaround for cursor issues
-  # };
+  # Recommended for Wayland (Hyprland, Sway, etc.)
+  environment.variables = {
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";  # workaround for cursor issues
+  };
 
   services.xserver.inputClassSections = [
     {
