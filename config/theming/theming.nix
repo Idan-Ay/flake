79e213b313132b166e6f655a44c6410dadaa36b3
Idan-Ay <idan.ayache@gmail.com>
2025-08-22 17:01:32 +0200

    Fixed border-radius

--- config/theming/theming.nix
+++ config/theming/theming.nix
@@ -23,8 +23,5 @@
     };
   };
 
-  xdg.configFile {
-    "gtk-3.0/gtk.css".source = ./gtk3.css;
-    "gtk-4.0/gtk.css".source = ./gtk4.css;
-  }
+  xdg.configFile."gtk-3.0/gtk.css".source = ./gtk3.css;
 }
