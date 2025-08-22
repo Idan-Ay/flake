e03c1ba6d91b245f1b5d1d8764b9eed8e7dcebec
Idan-Ay <idan.ayache@gmail.com>
2025-08-22 16:45:33 +0200

    Added gkt 4 css setting

--- config/theming/theming.nix
+++ config/theming/theming.nix
@@ -23,5 +23,8 @@
     };
   };
 
-  xdg.configFile."gtk-3.0/gtk.css".source = ./gtk3.css;
+  xdg.configFile {
+    "gtk-3.0/gtk.css".source = ./gtk3.css;
+    "gtk-4.0/gtk.css".source = ./gtk4.css;
+  }
 }
