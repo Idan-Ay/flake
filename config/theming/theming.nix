c552a924a4d3109c638e2cb6d4a44d33c974710c
Idan-Ay <idan.ayache@gmail.com>
2025-12-17 14:54:33 +0100

    Testing blackandwhite theme

--- config/theming/theming.nix
+++ config/theming/theming.nix
@@ -11,23 +11,19 @@
 
   gtk = {
     enable = true;
-    theme = {
-      name = "Orchis-Grey-Dark";
-      package = pkgs.orchis-theme.override {
-        tweaks = [ "black" ];
-      };
-    };
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
   };
 
-  xdg.configFile."gtk-3.0/gtk.css".source = ./gtk3.css;
-  xdg.configFile."gtk-4.0/gtk.css".source = ./gtk4.css;
+  xdg.dataFile."BlackAndWhite".source = .local/share/themes;
 
-  xdg.configFile."gtk-4.0/assets" = {
-    source = ./assets4;
-    recursive = true;
-  };
+  # xdg.configFile."gtk-3.0/gtk.css".source = ./gtk3.css;
+  # xdg.configFile."gtk-4.0/gtk.css".source = ./gtk4.css;
+
+  # xdg.configFile."gtk-4.0/assets" = {
+  #   source = ./assets4;
+  #   recursive = true;
+  # };
 }
