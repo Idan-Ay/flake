0c8498983ed1d93b690bb28503b3acdc99841e33
Idan-Ay <idan.ayache@proton.me>
2026-06-08 15:27:44 +0200

    removed ai from macbook, takes too many resources

--- packages/mac/latest-mac.nix
+++ packages/mac/latest-mac.nix
@@ -1,26 +1,6 @@
 { pkgsLatest-arm, lib, niri, ... }:
 
 {
-  services.ollama = {
-    enable = true;
-    package = pkgsLatest-arm.ollama-vulkan;
-  };
-  services.open-webui = {
-    enable = true;
-    openFirewall = false;
-    environment = {
-      ANONYMIZED_TELEMETRY = "False";
-      DO_NOT_TRACK = "True";
-      SCARF_NO_ANALYTICS = "True";
-      WEBUI_AUTH = "False";
-      ENABLE_WEB_SEARCH = "True";
-      WEB_SEARCH_ENGINE = "searxng";
-      SEARXNG_QUERY_URL = "https://search.iayache.com/search?q=<query>&format=json";
-      WEB_SEARCH_RESULT_COUNT = "5";
-      WEB_SEARCH_CONCURRENT_REQUESTS = "24";
-    };
-  };
-
   environment.systemPackages = lib.mkAfter (with pkgsLatest-arm; [
     niri.packages.aarch64-linux.default
     swayidle
